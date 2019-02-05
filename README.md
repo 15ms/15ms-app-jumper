872e785e7e81db6a1ccae0b0505650b1baf2497b
sartrey <sartrey@163.com>
2019-02-05 22:45:13 +0800

    0.4.0, edit secure hash algorithm, support kill name, add tests

--- README.md
+++ README.md
@@ -4,10 +4,24 @@ A domain name access HTTP API server.
 
 ## Usage
 
-### bind name with host
-http://15ms/?verb=bind&data=name,host
+### APIs
 
-### jump host by name
+#### bind name with host
+http://15ms/?verb=bind&data=name,host,code&hash=
+
+#### kill name
+http://15ms/?verb=kill&data=name,code&hash=
+
+#### find name
+http://15ms/?verb=find&data=name&hash=
+
+#### list name
+http://15ms/?verb=list&hash=
+
+#### hash
+substr(hex(sha256(JSON({ verb, data, time = YYYY-MM-DD HH:mm }))), HH * 2, 18)
+
+### Jump
 http://15ms/name...
 
 **localhost is invalid**
--- test/index.js
+++ test/index.js
@@ -14,6 +14,21 @@ function concatAPI(verb, data, hash) {
   return url;
 }
 
+function assertStatus(status) {
+  return function (response) {
+    assert.equal(response.status, status);
+    return response;
+  }
+}
+
+function toJSON(response) {
+  return response.json();
+}
+
+function toText(response) {
+  return response.text();
+}
+
 const server = new Server({
   store: path.join(__dirname, './fixtures'),
   token: 'b9b90db866c64ed69fad8aa395b5c149'
@@ -26,8 +41,7 @@ server.listen(8080);
 describe('APIs', () => {
   it('call without verb, should done', () => {
     const hash = secure.hashAPI('', '');
-    return fetch(concatAPI('', '', hash))
-      .then(response => response.text())
+    return fetch(concatAPI('', '', hash)).then(toText)
       .then(text => {
         assert.equal(text, '15ms\n');
       });
@@ -35,25 +49,18 @@ describe('APIs', () => {
 
   it('call without hash, should fail', () => {
     return fetch(concatAPI('find', 'abc'))
-      .then(response => {
-        assert.equal(response.status, 401);
-      });
+      .then(assertStatus(401));
   });  
 
   it('call with error hash, should fail', () => {
     return fetch(concatAPI('find', 'abc', '1234abcd'))
-      .then(response => {
-        assert.equal(response.status, 403);
-      });
+      .then(assertStatus(403));
   });  
 
   it('bind name with invalid host - 1, should fail', () => {
     const hash = secure.hashAPI('bind', 'abc,localhost:8080');
     return fetch(concatAPI('bind', 'abc,localhost:8080', hash))
-      .then(response => {
-        assert.equal(response.status, 500);
-        return response.json();
-      })
+      .then(assertStatus(500)).then(toJSON)
       .then(json => {
         assert.deepEqual(json, { state: false, error: 'invalid host' });
       });
@@ -62,10 +69,7 @@ describe('APIs', () => {
   it('bind name with invalid host - 2, should fail', () => {
     const hash = secure.hashAPI('bind', 'abc,127.0.0.1:9999');
     return fetch(concatAPI('bind', 'abc,127.0.0.1:9999', hash))
-      .then(response => {
-        assert.equal(response.status, 500);
-        return response.json();
-      })
+      .then(assertStatus(500)).then(toJSON)
       .then(json => {
         assert.deepEqual(json, { state: false, error: 'invalid host' });
       });
@@ -73,68 +77,123 @@ describe('APIs', () => {
 
   it('bind name with host, should done', () => {
     const hash1 = secure.hashAPI('bind', 'a,a1.test.com');
+    const hash2 = secure.hashAPI('find', 'a');
     return fetch(concatAPI('bind', 'a,a1.test.com', hash1))
-      .then(response => response.json())
+      .then(assertStatus(200)).then(toJSON)
       .then(json1 => {
         assert.deepEqual(json1, { state: true });
-        const hash2 = secure.hashAPI('find', 'a');
         return fetch(concatAPI('find', 'a', hash2))
-          .then(response => response.json())
+          .then(assertStatus(200)).then(toJSON)
           .then(json2 => {
             assert.deepEqual(json2, { state: true, model: 'a1.test.com' });
           });
       });
   });
 
-  it('bind name with old host, should done, cover more code', () => {
+  it('bind name with old host, should done, for coverage', () => {
     const hash1 = secure.hashAPI('bind', 'a,a1.test.com');
-    return fetch(concatAPI('bind', 'a,a1.test.com', hash1))
-      .then(response => response.json())
+    const hash2 = secure.hashAPI('find', 'a');
+    return fetch(concatAPI('bind', 'a,a1.test.com', hash1)).then(toJSON)
       .then(json1 => {
         assert.deepEqual(json1, { state: true });
-        const hash2 = secure.hashAPI('find', 'a');
-        return fetch(concatAPI('find', 'a', hash2))
-          .then(response => response.json())
+        return fetch(concatAPI('find', 'a', hash2)).then(toJSON)
           .then(json2 => {
             assert.deepEqual(json2, { state: true, model: 'a1.test.com' });
           });
       });
   });
 
-  it('bind name with new host, should done, cover more code', () => {
+  it('bind name with new host, should done, for coverage', () => {
     const hash1 = secure.hashAPI('bind', 'a,a2.test.com');
-    return fetch(concatAPI('bind', 'a,a2.test.com', hash1))
-      .then(response => response.json())
+    const hash2 = secure.hashAPI('find', 'a');
+    return fetch(concatAPI('bind', 'a,a2.test.com', hash1)).then(toJSON)
       .then(json1 => {
         assert.deepEqual(json1, { state: true });
-        const hash2 = secure.hashAPI('find', 'a');
-        return fetch(concatAPI('find', 'a', hash2))
-          .then(response => response.json())
+        return fetch(concatAPI('find', 'a', hash2)).then(toJSON)
           .then(json2 => {
             assert.deepEqual(json2, { state: true, model: 'a2.test.com' });
           });
       });
   });
 
-  it('bind more name with host, should done', () => {
-    const hash1 = secure.hashAPI('bind', 'b,b1.test.com');
-    return fetch(concatAPI('bind', 'b,b1.test.com', hash1))
-      .then(response => response.json())
+  it('bind more name with host and code, should done', () => {
+    const hash1 = secure.hashAPI('bind', 'b,b1.test.com,123');
+    const hash2 = secure.hashAPI('find', 'b');
+    return fetch(concatAPI('bind', 'b,b1.test.com,123', hash1)).then(toJSON)
       .then(json1 => {
         assert.deepEqual(json1, { state: true });
-        const hash2 = secure.hashAPI('find', 'b');
-        return fetch(concatAPI('find', 'b', hash2))
-          .then(response => response.json())
+        return fetch(concatAPI('find', 'b', hash2)).then(toJSON)
           .then(json2 => {
             assert.deepEqual(json2, { state: true, model: 'b1.test.com' });
           });
       });
   });
 
+  it('bind name with new host and old code, should done', () => {
+    const hash1 = secure.hashAPI('bind', 'b,b2.test.com,123');
+    const hash2 = secure.hashAPI('find', 'b');
+    return fetch(concatAPI('bind', 'b,b2.test.com,123', hash1)).then(toJSON)
+      .then(json1 => {
+        assert.deepEqual(json1, { state: true });
+        return fetch(concatAPI('find', 'b', hash2)).then(toJSON)
+          .then(json2 => {
+            assert.deepEqual(json2, { state: true, model: 'b2.test.com' });
+          });
+      });
+  });
+
+  it('bind name with new host and new code, should fail', () => {
+    const hash1 = secure.hashAPI('bind', 'b,b3.test.com,456');
+    const hash2 = secure.hashAPI('find', 'b');
+    return fetch(concatAPI('bind', 'b,b3.test.com,456', hash1))
+      .then(assertStatus(500)).then(toJSON)
+      .then(json1 => {
+        assert.deepEqual(json1, { state: false, error: 'invalid code' });
+        return fetch(concatAPI('find', 'b', hash2)).then(toJSON)
+          .then(json2 => {
+            assert.deepEqual(json2, { state: true, model: 'b2.test.com' });
+          });
+      });
+  });
+
+  it('bind name for kill test, should done', () => {
+    const hash1 = secure.hashAPI('bind', 'c,c1.test.com,123');
+    const hash2 = secure.hashAPI('find', 'c');
+    return fetch(concatAPI('bind', 'c,c1.test.com,123', hash1))
+      .then(assertStatus(200)).then(() => {
+        return fetch(concatAPI('find', 'c', hash2))
+          .then(assertStatus(200)).then(toJSON)
+          .then((json) => {
+            assert.deepEqual(json, { state: true, model: 'c1.test.com' });
+          });
+      });
+  });
+
+  it('kill name with new code, should fail', () => {
+    const hash1 = secure.hashAPI('kill', 'c,456');
+    const hash2 = secure.hashAPI('find', 'c');
+    return fetch(concatAPI('kill', 'c,456', hash1))
+      .then(assertStatus(500)).then(toJSON)
+      .then(json => {
+        assert.deepEqual(json, { state: false, error: 'invalid code' });
+        return fetch(concatAPI('find', 'c', hash2)).then(assertStatus(200));
+      });
+  });
+
+  it('kill name with old code, should done', () => {
+    const hash1 = secure.hashAPI('kill', 'c,123');
+    const hash2 = secure.hashAPI('find', 'c');
+    return fetch(concatAPI('kill', 'c,123', hash1))
+      .then(assertStatus(200)).then(toJSON)
+      .then(json => {
+        assert.deepEqual(json, { state: true });
+        return fetch(concatAPI('find', 'c', hash2)).then(assertStatus(404));
+      });
+  });
+
   it('list names, should done', () => {
     const hash = secure.hashAPI('list', '');
-    return fetch(concatAPI('list', '', hash))
-      .then(response => response.json())
+    return fetch(concatAPI('list', '', hash)).then(toJSON)
       .then(json => {
         assert.deepEqual(json, { state: true, model: ['a', 'b'] });
       });
@@ -142,8 +201,7 @@ describe('APIs', () => {
 
   it('find name, null name, should fail', () => {
     const hash = secure.hashAPI('find', '');
-    return fetch(concatAPI('find', '', hash))
-      .then(response => response.json())
+    return fetch(concatAPI('find', '', hash)).then(toJSON)
       .then(json => {
         assert.deepEqual(json.state, false);
       });
@@ -151,8 +209,7 @@ describe('APIs', () => {
 
   it('find name, should fail', () => {
     const hash = secure.hashAPI('find', 'c');
-    return fetch(concatAPI('find', 'c', hash))
-      .then(response => response.json())
+    return fetch(concatAPI('find', 'c', hash)).then(toJSON)
       .then(json => {
         assert.deepEqual(json.state, false);
       });
@@ -161,10 +218,7 @@ describe('APIs', () => {
 
 describe('Jump', () => {
   it('find name, new name, should fail', () => {
-    return fetch('http://localhost:8080/c')
-      .then(response => {
-        assert.equal(response.status, 404);
-      })
+    return fetch('http://localhost:8080/c').then(assertStatus(404));
   });
 
   it('find name, old name, should done', () => {
@@ -179,7 +233,7 @@ describe('Jump', () => {
     return fetch('http://localhost:8080/b/1/2.3/4-5?q1=1&q2=2', { redirect: 'manual' })
       .then(response => {
         assert.equal(response.status, 302);
-        assert.equal(response.headers.get('Location'), 'http://b1.test.com/1/2.3/4-5?q1=1&q2=2');
+        assert.equal(response.headers.get('Location'), 'http://b2.test.com/1/2.3/4-5?q1=1&q2=2');
       })
   });
 });
